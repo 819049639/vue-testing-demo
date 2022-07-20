c7c78fdb9faa9c5b433b47acd89bc2eaf80a01d1
王强 <wangqiang@feewee.cn>
2022-07-20 14:08:27 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,13 @@
  * @Author: wangqiang@feewee.cn
  * @Date: 2022-07-19 11:43:47
  * @LastEditors: wangqiang@feewee.cn
- * @LastEditTime: 2022-07-20 10:58:43
+ * @LastEditTime: 2022-07-20 14:08:20
  */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vue-testing-demo'
+  publicPath: '/vue-testing-demo',
+  devServer: {
+    historyApiFallback: true
+  }
 })
