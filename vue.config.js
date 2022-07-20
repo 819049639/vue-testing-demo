59563a18b589a6f111a8e4e2914b868f2e1741da
王强 <wangqiang@feewee.cn>
2022-07-20 11:03:05 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
+/*
+ * @Author: wangqiang@feewee.cn
+ * @Date: 2022-07-19 11:43:47
+ * @LastEditors: wangqiang@feewee.cn
+ * @LastEditTime: 2022-07-20 10:58:43
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/vue-testing-demo'
 })
